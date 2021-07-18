812639f6e41cd91101580049b8f2c311568acd23
ibisdavi012 <78810908+ibisdavi012@users.noreply.github.com>
2021-07-18 18:45:57 -0400

    View Template Fixes

--- index.js
+++ index.js
@@ -3,33 +3,37 @@ const mongoose = require('mongoose');
 const appConfig = require('./config/app.config');
 const routes = require('./routes');
 const app = express();
-const sampleDataLoader = require('./tools/sampleDataLoader')
+const sampleDataLoader = require('./tools/sampleDataLoader');
 const settings = require('./settings');
 
 settings(app);
 
 routes(app);
 
-mongoose.connect(appConfig.mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(appConfig.mongoUri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
 
-db.on('error', err => {
+db.on('error', (err) => {
 	console.log('Error');
 });
 
 db.once('open', () => {
-		app.listen(appConfig.port, () => {
+	app.listen(appConfig.port, () => {
 		// Server started
 		console.log(` Server is listening on port: ${appConfig.port}`);
 
 		// Copy JSON file to the database
-		sampleDataLoader.load().then(() => {
-			console.log("Sample Data was Copied")
-		}).catch(err => {
-			console.log(err)
-		});
+		sampleDataLoader
+			.load()
+			.then(() => {
+				console.log('Sample Data was Copied');
+			})
+			.catch((err) => {
+				console.log(err);
+			});
 	});
 });
-
-
